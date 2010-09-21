defb5566751e2303beafac46478e1c05e90cadfd
Pete Chudykowski <p@chudpi.org>
2010-09-20 17:23:09 -0700

    Corrected author identification entries.

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
     name         = 'approot',
     version      = '0.1.0',
     description  = 'On import finds the application root directory',
-    author       = 'Greg Ward',
-    author_email = 'gward@python.net'
+    author       = 'Pete Chudykowski',
+    author_email = 'p@chudpi.org',
     py_modules   = ['approot'],
 )
 
